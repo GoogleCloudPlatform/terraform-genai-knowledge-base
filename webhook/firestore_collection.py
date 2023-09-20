124a6b1df20d193bd1877930f18353ac9e2350d1
Eric Schmidt <erschmid@google.com>
2023-09-20 16:21:04 -0700

    feat: added Firestore component

---
+++ webhook/firestore_collection.py
@@ -0,0 +1,64 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from datetime import datetime
+from typing import List, Tuple
+
+from google.cloud import firestore
+
+
+def write_qas_to_collection(
+    project_id: str,
+    collection_name: str,
+    question_answer_pairs: List[Tuple[str, str]],
+    input_file_gcs_uri: str,
+    time_created: datetime,
+):
+    """Writes question and answer pairs to the specified Firestore collection.
+
+    Arguments:
+      project_id: the project that contains this database
+      collection_name: the collection to store the Q&A pairs in
+      question_answer_pairs: the Q&A pairs to add
+      input_file_gcs_uri: the Cloud Storage URI for the source PDF
+      time_created: the time that this PDF was uploaded
+    """
+    db = firestore.Client(project=project_id)
+    bulkwriter = db.bulk_writer()
+
+    for qa in question_answer_pairs:
+  
+        # Create a unique ID for each question
+        question_hash = hash(qa[0])
+
+        doc_ref = db.document(collection_name, str(question_hash))
+        doc_snap = doc_ref.get()
+
+        document_data = {
+            "question": qa[0],
+            "answers": [{
+                "answer": qa[1],
+                "gcs_uri": input_file_gcs_uri,
+                "time_uploaded": time_created,
+            }]
+        }
+
+        if doc_snap.exists:
+            bulkwriter.update(doc_ref, document_data)
+            continue
+
+        bulkwriter.create(doc_ref, document_data)
+
+    # Send all updates and close the BulkWriter
+    bulkwriter.close()
---
+++ webhook/unit_tests/firestore_collection_test.py
@@ -0,0 +1,86 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from datetime import datetime
+from unittest.mock import MagicMock, patch
+
+from google.cloud import firestore
+from firestore_collection import write_qas_to_collection
+
+_PROJECT_ID = "fake-project-id"
+_COLLECTION_NAME = "fake-collection"
+_GCS_URI = "gs://i/dont/exist"
+
+
+@patch.object(firestore.Client, "document")
+@patch.object(firestore.Client, "bulk_writer")
+def test_write_qas_to_collection(mock_bulkwriter, mock_document):
+    # Arrange
+    mock_document_ref = MagicMock(spec=firestore.DocumentReference)
+    mock_document.return_value = mock_document_ref
+    mock_snapshot = MagicMock(spec=firestore.DocumentSnapshot, exists=False)
+    mock_document_ref.get.return_value = mock_snapshot
+    mock_bulkwriter_ref = MagicMock()
+    mock_bulkwriter.return_value = mock_bulkwriter_ref
+
+    timestamp = datetime.now()
+    data_row = ("Who is the Greek goddess of the hunt?", "Artemis")
+
+    # Act
+    write_qas_to_collection(
+        _PROJECT_ID,
+        _COLLECTION_NAME,
+        [data_row],
+        _GCS_URI,
+        timestamp)
+
+    # Assert
+    mock_document.assert_called_with(_COLLECTION_NAME, str(hash(data_row[0])))
+    mock_document_ref.get.assert_called()
+    mock_bulkwriter_ref.create.assert_called()
+    mock_bulkwriter_ref.close.assert_called()
+
+
+@patch.object(firestore.Client, "document")
+@patch.object(firestore.Client, "bulk_writer")
+def test_write_multiple_qas_to_collection(mock_bulkwriter, mock_document):
+    # Arrange
+    mock_document_ref = MagicMock(spec=firestore.DocumentReference)
+    mock_document.return_value = mock_document_ref
+    mock_snapshot_1 = MagicMock(spec=firestore.DocumentSnapshot, exists=False)
+    mock_snapshot_2 = MagicMock(spec=firestore.DocumentSnapshot, exists=True)
+    mock_document_ref.get.side_effect = [mock_snapshot_1, mock_snapshot_2]
+    mock_bulkwriter_ref = MagicMock()
+    mock_bulkwriter.return_value = mock_bulkwriter_ref
+
+    timestamp = datetime.now()
+    data_rows = [
+        ("Who is the Greek goddess of the hunt?", "Artemis"),
+        ("Which Israelite prophet lived at the time of King Ahab?", "Elijah"),
+    ]
+
+    # Act
+    write_qas_to_collection(
+        _PROJECT_ID,
+        _COLLECTION_NAME,
+        data_rows,
+        _GCS_URI,
+        timestamp)
+
+    # Assert
+    mock_document.assert_called()
+    mock_document_ref.get.assert_called()
+    mock_bulkwriter_ref.create.assert_called()
+    mock_bulkwriter_ref.update.assert_called()
+    mock_bulkwriter_ref.close.assert_called()
