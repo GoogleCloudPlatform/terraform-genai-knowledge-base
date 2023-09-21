1631497d41b7edbe93e12a2a839f473114bc5c24
Eric Schmidt <erschmid@google.com>
2023-09-21 12:49:08 -0700

    test: added Firestore component test

--- webhook/integration_tests/firestore_collection_test.py
+++ webhook/integration_tests/firestore_collection_test.py
@@ -11,3 +11,59 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import backoff
+import os
+from datetime import datetime
+
+from google.cloud import firestore
+
+from firestore_collection import write_qas_to_collection
+
+
+_PROJECT_ID = os.environ["PROJECT_ID"]
+# Make sure this is a test collection. It is entirely deleted in teardown.
+_COLLECTION_NAME = os.environ["COLLECTION"]
+
+
+@backoff.on_exception(backoff.expo, Exception, max_tries=3)
+def clean_collection(client: firestore.Client,
+                     coll_ref: firestore.CollectionReference):
+    client.recursive_delete(coll_ref)
+
+
+@backoff.on_exception(backoff.expo, Exception, max_tries=3)
+def test_write_qas_to_collection_it(capsys):
+    # Arrange
+    timestamp = datetime.now()
+    question_1 = "Who is the Greek goddess of the hunt?"
+    question_2 = "Which Israelite prophet lived at the time of King Ahab?"
+    data_rows = [
+        (question_1, "Artemis"),
+        (question_2, "Elijah"),
+    ]
+    gcs_uri = "gs://i/am/a/test.pdf"
+
+    client = firestore.Client()
+    collection = client.collection(_COLLECTION_NAME)
+
+    # Act
+    try:
+        write_qas_to_collection(
+            project_id=_PROJECT_ID,
+            collection_name=_COLLECTION_NAME,
+            question_answer_pairs=data_rows,
+            input_file_gcs_uri=gcs_uri,
+            time_created=timestamp)
+
+        # Assert
+        assert capsys.readouterr().out == ""
+        got_1 = collection.document(str(hash(question_1))).get()
+        got_2 = collection.document(str(hash(question_2))).get()
+
+        assert got_1.exists
+        assert got_2.exists
+
+    finally:
+        # Clean up
+        clean_collection(client, collection)
