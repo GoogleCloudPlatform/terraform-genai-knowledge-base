7e65d52e3f72c6d78f033d905c9b55b39d26c317
David Cavazos <dcavazos@google.com>
2023-12-19 16:35:58 -0800

    Finalize metadata for JSS (#38)
    * define automatic variables
    
    Change-Id: I3320f5736557855f0a22371b55f21ec7e9654c0b
    
    * make resources unique always
    
    Change-Id: I6b9823cf921172d8d130dd927e69ed495b07185e
    
    * use unique names for tests
    
    Change-Id: I8e0b2ddd05f2371c1319e1b4ab2598b20326c6c7
    
    * fix webhook region
    
    Change-Id: Id72aab90a29e045a0f57ec6edbe5b5790e681d38
    
    * fix firestore name
    
    Change-Id: Id580721ea9112f12aa01df7310ee085b541fc37d
    
    * remove delete_contents_on_destroy
    
    Change-Id: I3bb98e929750ea94c8a6f327009d50a80ba89100
    
    * generate_docs
    
    Change-Id: Idb8a32f26ae4f9b2b5799f25632ffc0a1999fd9a
    
    * hide unnecessary input fields
    
    Change-Id: I96e82fb01f961ca8177b74cea9ff9fa45e509533

--- webhook/test_e2e.py
+++ webhook/test_e2e.py
@@ -18,7 +18,6 @@ import json
 import os
 import pytest
 import subprocess
-import uuid
 from typing import Any
 
 from google.cloud import firestore
@@ -28,7 +27,6 @@ import firestore_utils
 from process_document import DATASET_COLLECTION, OUTPUT_NAME, process_document
 
 PROJECT_ID = os.environ["PROJECT_ID"]
-UUID = os.environ.get("TEST_UUID", uuid.uuid4().hex[:6])
 
 
 def run_cmd(*cmd: str, **kwargs: Any) -> subprocess.CompletedProcess:
@@ -37,20 +35,9 @@ def run_cmd(*cmd: str, **kwargs: Any) -> subprocess.CompletedProcess:
 
 
 @pytest.fixture(scope="session")
-def resources() -> Iterator[dict]:
+def outputs() -> Iterator[dict[str, str]]:
     print("---")
     print(f"{PROJECT_ID=}")
-    print(f"{UUID=}")
-    resources = {
-        "bucket_main": os.environ.get("TEST_BUCKET_MAIN", f"{PROJECT_ID}-{UUID}"),
-        "documentai_processor_name": os.environ.get(
-            "TEST_DOCUMENTAI_PROCESSOR_NAME", f"test-webhook-{UUID}"
-        ),
-        "firestore_name": os.environ.get(
-            "TEST_FIRESTORE_DATABASE_NAME", f"test-webhook-{UUID}"
-        ),
-    }
-    print(f"resources={json.dumps(resources, indent=2)}")
     if not os.environ.get("TEST_SKIP_INIT"):
         run_cmd("terraform", "-chdir=..", "init", "-input=false")
     if not os.environ.get("TEST_SKIP_APPLY"):
@@ -61,12 +48,17 @@ def resources() -> Iterator[dict]:
             "-input=false",
             "-auto-approve",
             f"-var=project_id={PROJECT_ID}",
-            *[f"-var={name}={value}" for name, value in resources.items()],
             "-target=google_storage_bucket.main",
             "-target=google_document_ai_processor.ocr",
-            "-target=google_firestore_database.database",
+            "-target=google_firestore_database.main",
         )
-    yield resources
+    p = run_cmd("terraform", "-chdir=..", "output", "-json", stdout=subprocess.PIPE)
+    outputs = {
+        name: value["value"]
+        for name, value in json.loads(p.stdout.decode("utf-8")).items()
+    }
+    print(f"{outputs=}")
+    yield outputs
     if not os.environ.get("TEST_SKIP_DESTROY"):
         run_cmd(
             "terraform",
@@ -78,41 +70,30 @@ def resources() -> Iterator[dict]:
         )
 
 
-@pytest.fixture(scope="session")
-def outputs(resources: dict) -> dict[str, str]:
-    p = run_cmd("terraform", "-chdir=..", "output", "-json", stdout=subprocess.PIPE)
-    outputs = {
-        name: value["value"]
-        for name, value in json.loads(p.stdout.decode("utf-8")).items()
-    }
-    print(f"{outputs=}")
-    return outputs
-
-
-def test_end_to_end(resources: dict, outputs: dict[str, str]) -> None:
+def test_end_to_end(outputs: dict[str, str]) -> None:
     print(">> process_document")
     process_document(
-        event_id=f"webhook-test-{UUID}",
+        event_id=f"webhook-test-{outputs['unique_id']}",
         input_bucket="arxiv-dataset",
         input_name="arxiv/cmp-lg/pdf/9410/9410009v1.pdf",
         mime_type="application/pdf",
         time_uploaded=datetime.datetime.now(),
         docai_prcessor_id=outputs["documentai_processor_id"],
-        output_bucket=resources["bucket_main"],
-        database=resources["firestore_name"],
+        output_bucket=outputs["bucket_main_name"],
+        database=outputs["firestore_database_name"],
         force_reprocess=True,
     )
 
     # Make sure we have a non-empty dataset.
     print(">> Checking output bucket")
-    with storage_utils.read(resources["bucket_main"], OUTPUT_NAME) as f:
+    with storage_utils.read(outputs["bucket_main_name"], OUTPUT_NAME) as f:
         lines = [line.strip() for line in f]
         print(f"dataset {len(lines)=}")
         assert len(lines) > 0, "expected a non-empty dataset in the output bucket"
 
     # Make sure the Firestore database is populated.
     print(">> Checking Firestore database")
-    db = firestore.Client(database=resources["firestore_name"])
+    db = firestore.Client(database=outputs["firestore_database_name"])
     entries = list(firestore_utils.read(db, DATASET_COLLECTION))
     print(f"database {len(entries)=}")
     assert len(entries) == len(lines), "database entries do not match the dataset"
