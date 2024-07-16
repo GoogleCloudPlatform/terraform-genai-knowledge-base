1ac25438ad378313aa54a12b879cfe4291c75c84
Nim Jayawardena <i.am.nim.jay@gmail.com>
2024-07-16 16:49:22 -0400

    fix: Pin Google provider to 5.37.0 & set LOG_EXECUTION_ID (#166)

--- versions.tf
+++ versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.8"
+      version = "= 5.37.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 5.8"
+      version = "= 5.37.0"
     }
     archive = {
       source  = "hashicorp/archive"
